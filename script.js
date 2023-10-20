3985c5f6074afe91f31adb078c724d5af9ab8399
Mike-369 <123855871+michelperea@users.noreply.github.com>
2023-10-19 18:27:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Selecciona todos los elementos de pantalla y los botones de selección de insectos
 const screens = document.querySelectorAll('.screen');
 const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
 const start_btn = document.getElementById('start-btn')
@@ -9,24 +10,30 @@ let seconds = 0
 let score = 0
 let selected_insect = {}
 
+// Agrega un event listener al botón de inicio
 start_btn.addEventListener('click', () => screens[0].classList.add('up'))
 
+// Agrega event listeners a los botones de selección de insectos
 choose_insect_btns.forEach(btn => {
     btn.addEventListener('click', () => {
+        // Obtiene la imagen y la información del insecto seleccionado
         const img = btn.querySelector('img')
         const src = img.getAttribute('src')
         const alt = img.getAttribute('alt')
         selected_insect = { src, alt }
+        // Oculta la pantalla de selección y comienza el juego
         screens[1].classList.add('up')
         setTimeout(createInsect, 1000)
         startGame()
     })
 })
 
+// Inicia el juego y comienza a medir el tiempo
 function startGame() {
     setInterval(increaseTime, 1000)
 }
 
+// Incrementa el tiempo y lo muestra en el elemento de tiempo
 function increaseTime() {
     let m = Math.floor(seconds / 60)
     let s = seconds % 60
@@ -36,12 +43,14 @@ function increaseTime() {
     seconds++
 }
 
+// Crea un nuevo insecto en una ubicación aleatoria
 function createInsect() {
     const insect = document.createElement('div')
     insect.classList.add('insect')
     const { x, y } = getRandomLocation()
     insect.style.top = `${y}px`
     insect.style.left = `${x}px`
+    // Crea la imagen del insecto y la rota aleatoriamente
     insect.innerHTML = `<img src="${selected_insect.src}" alt="${selected_insect.alt}" style="transform: rotate(${Math.random() * 360}deg)" />`
 
     insect.addEventListener('click', catchInsect)
@@ -49,6 +58,7 @@ function createInsect() {
     game_container.appendChild(insect)
 }
 
+// Obtiene una ubicación aleatoria en la ventana del navegador
 function getRandomLocation() {
     const width = window.innerWidth
     const height = window.innerHeight
@@ -57,6 +67,7 @@ function getRandomLocation() {
     return { x, y }
 }
 
+// Maneja el evento de hacer clic en un insecto (atrapar un insecto)
 function catchInsect() {
     increaseScore()
     this.classList.add('caught')
@@ -64,11 +75,13 @@ function catchInsect() {
     addInsects()
 }
 
+// Agrega insectos adicionales después de un tiempo
 function addInsects() {
     setTimeout(createInsect, 1000)
     setTimeout(createInsect, 1500)
 }
 
+// Incrementa la puntuación y muestra un mensaje si se supera cierto puntaje
 function increaseScore() {
     score++
     if(score > 19) {
